85198e1a41997097af99e2b7a9b5e4cca5445fba
Phillip Ngan <phillip.ngan@gmail.com>
2021-07-03 20:46:40 +1200

    Initial commit for application to generate load in a kubernetes cluster

---
+++ aspnetcore-kubernetes/Controllers/LoadController.cs
@@ -0,0 +1,26 @@
+﻿using aspnetcore_kubernetes.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace aspnetcore_kubernetes.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class LoadController : Controller
+    {
+        [HttpGet]
+        [Route("ping")]
+        public string Ping()
+        {
+            return "Pong";
+        }
+
+        [HttpGet]
+        [Route("info")]
+        public string Info()
+        {
+            var model = new HomePageViewModel();
+            return JsonSerializer.Serialize(model);
+        }
+    }
+}
---
+++ aspnetcore-kubernetes/Models/HomePageViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+
+namespace aspnetcore_kubernetes.Models
+{
+    public sealed class HomePageViewModel
+    {
+        public string IpAddress
+        {
+            get
+            {
+                return GetLocalIPAddress();
+            }
+        }
+        public string HostName
+        {
+            get
+            {
+                return GetLocalHostname();
+            }
+        }
+
+        public string Application
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Name;
+            }
+        }
+        public string Version
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        private static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private static string GetLocalHostname()
+        {
+            return Dns.GetHostName();
+        }
+    }
+}
